92260e76f3e4b921bd44b43be8cf75fde294bcba
Rubio <jonrubio92@gmail.com>
2023-05-13 16:11:59 -0400

    Prints out how to run program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::cout << "Welcome to the restaurant simulator!" << std::endl;
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " <input file> <number of tables available>" << std::endl;
+        std::cout << "The input file should contain a list of numbers, 4 per line in the following format:" << std::endl;
+        std::cout << "<time arrived> <time to finish eating> <time willing to wait for table> <revenue>" << std::endl;
+        return 1;
+        }
     return 0;
 }
