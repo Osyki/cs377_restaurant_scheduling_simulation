47b60950545b87e4c5b93533da25318668e25282
eky <jon.rubio.92@gmail.com>
2023-05-16 17:32:21 -0400

    Began writing comments to sjf.cpp

--- src/sjf.cpp
+++ src/sjf.cpp
@@ -1,23 +1,32 @@
 #include "sjf.h"
 #include <unistd.h>
 
+/**
+ * Reads in a file and sets up thread to call policy.
+ * @param filename: The file to read in.
+ * @param num_tables: The the number of threads to spawn.
+*/
 SJF::SJF(const std::string &filename, const int &num_tables)
 {
+    // reads in file and sets number of threads
     read_workload(filename);
     this->xs = get_job_queue();
     this->num_tables = num_tables;
     for (int i = 0; i < num_tables; i++)
     {
+        // set each thread to call run_policy
         this->threads.emplace_back(&SJF::run_policy, this);
     }
     sleep(1); // sleep for 1 second to wait for all threads to initialize
     cout_lock.lock();
-    std::cout << "\n**All threads initialized.. beginning work**\n"
-              << std::endl;
+    std::cout << "\n**All threads initialized.. beginning work**\n" << std::endl;
     cout_lock.unlock();
-    pthread_cond_broadcast(&cond);
+    pthread_cond_broadcast(&cond); // release the condition to all threads to begin execution
 }
 
+/**
+ * The scheduling policy.
+*/
 void SJF::run_policy()
 {
     Metrics thread_metrics;
