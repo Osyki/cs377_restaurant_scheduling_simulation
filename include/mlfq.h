881accc243e436e01e5d0b26dd765085a3680b42
eky <jon.rubio.92@gmail.com>
2023-05-16 14:28:43 -0400

    Added MLFQ skeleton.

---
+++ include/mlfq.h
@@ -0,0 +1,19 @@
+#ifndef MLFQ_H
+#define MLFQ_H
+
+#include "scheduling_policy.h"
+
+class MLFQ : public SchedulingPolicy
+{
+public:
+    MLFQ(const std::string &workload_filename, const int &num_tables);
+    ~MLFQ();
+
+    void run_policy();
+
+private:
+    pqueue_arrival xs; // copy of job queue
+
+};
+
+#endif // MLFQ_H
